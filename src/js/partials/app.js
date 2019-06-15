53632a653c9f1fe5f29a8a3fa7a311a72a8cc45c
Shumovka <mr.cool.amelin@mail.ru>
2019-06-16 00:55:59 +0600

    pre-release

---
+++ src/js/partials/app.js
@@ -0,0 +1,6 @@
+$(".card__toggle img").bind("click", function() {
+      var src = ($(this).attr("src") === "../../img/card/icons/blue-heart.png")
+                    ? "../../img/card/icons/heart.png" 
+                    : "../../img/card/icons/blue-heart.png";
+      $(this).attr("src", src);
+});
